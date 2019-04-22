a81af037da7288ce11dbde585f33c33f2583f346
bailerong <15929276532@163.com>
2019-04-22 23:02:16 +0800

    我们的固定线程数的线程池

---
+++ FixedThreadPoolTest.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.*;
+
+public class FixedThreadPoolTest {
+    public static ExecutorService newFixedThreadPool(int nThreads){
+        return new ThreadPoolExecutor(nThreads,nThreads,0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+    }
+    public static void main(String[] args) {
+
+        ExecutorService executorService=Executors.newFixedThreadPool(5);
+        for(int i=0;i<5;i++){
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    for(int j=0;j<10;j++){
+                        System.out.println(Thread.currentThread().getName()+" 、"+j);
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
